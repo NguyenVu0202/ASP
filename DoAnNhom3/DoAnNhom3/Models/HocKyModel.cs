20b0ee32e4474d0e0c66ed5ac02e194420b28a5b
thuytiendev <thuytien16102003@gmail.com>
2024-10-26 23:15:58 +0700

    ver 1.0

---
+++ DoAnNhom3/DoAnNhom3/Models/HocKyModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoAnNhom3.Models
+{
+    public class HocKyModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string TenHK { get; set; }
+        public virtual ICollection<TaiLieuModel> TaiLieu { get; set; }
+    }
+}
---
+++ DoAnNhom3/DoAnNhom3/Models/TaiLieuModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoAnNhom3.Models
+{
+    public class TaiLieuModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int KhoaId { get; set; }
+        public int HocKyId { get; set; }
+        public string TenTaiLieu { get; set; }
+        public string HinhTaiLieu { get; set; }
+        public decimal Gia { get; set; }
+        public virtual KhoaModel Khoa { get; set; }
+        public virtual HocKyModel HocKy { get; set; }
+    }
+}
